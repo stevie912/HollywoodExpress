5fb401afbe2f5821d85b0daa56592e234e989040
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-22 20:41:45 +0000

    created basic home page with movie search forms

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,15 +1,49 @@
 <?php require_once 'app/views/templates/header.php' ?>
 <div class="container">
     <div class="page-header" id="banner">
-        <div class="row">
+        <div class="row justify-content-center text-center">
             <div class="col-lg-12 text-center">
                 <br>
-                <h1 class="display-1">Oh, hey there <?php echo $_SESSION['username'] ?>!</h1>
+                <h1 class="display-1">Hollywood Express</h1>
+                <p class="display-6">The movie search and recommendation engine</p>
                 <br>
-                <p class="display-6">Did you know that today is <?= date("F jS, Y"); ?>?</p>
+            </div>
+                
+            <div class="col-sm-6 text-center">
+                <p class="lead">Search a movie title:</p>
+                <form action="/search" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Movie title" aria-label="Movie title" aria-describedby="button-addon2" name="title">
+                        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                    </div>
+                </form>
                 <br>
-                <p class="lead">Come on in, sit right down (no you're not the first to show),</p>
-                <p class="lead">maybe leave yourself a reminder!</p>
+                <br>
+            </div>    
+                
+            <p class="lead">Or, enter the titles of three favourite movies and we'll recommend a movie we think you'll like:</p>
+            
+            <div class="col-sm-6">
+                <form action="/recommend" method="post">
+                    <fieldset>
+                      <div class="form-group">
+                        <label for="Rtitle1">Movie 1:</label>
+                        <input required type="text" id="Rtitle1" class="form-control" name="Rtitle1">
+                      </div>
+                      <div class="form-group">
+                        <label for="Rtitle2">Movie 2:</label>
+                        <input required type="text" id="Rtitle2" class="form-control" name="Rtitle2">
+                      </div>
+                      <div class="form-group">
+                        <label for="Rtitle3">Movie 3:</label>
+                        <input required type="text" id="Rtitle3" class="form-control" name="Rtitle3">
+                      </div>
+                        <br>
+                        <button type="submit" value ="submit" class="btn btn-primary">Submit</button>
+                    </fieldset>
+                </form>
+            </div>
+                    
             </div>
         </div>
     </div>
