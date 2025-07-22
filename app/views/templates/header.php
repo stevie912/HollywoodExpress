7e91226f8d7bd6e5d1c00f9d7a3fe5acb6ac1dc8
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-22 20:17:39 +0000

    Modified header to display home page without requiring login, and updated meta details

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,35 +1,28 @@
-<?php
-if (!isset($_SESSION['auth'])) {
-    header('Location: /login');
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en" >
     <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 
         <link rel="icon" href="/favicon.png">
-        <title>COSC 4806</title>
+        <title>Hollywood Express</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="mobile-web-app-capable" content="yes">
-        <meta name="description" content="web application for COSC 4806 assignment">
+        <meta name="description" content="Hollywood Express is a movie search engine that lets you search for movie info, leave a rating for a movie, or discover new favourites.">
         <meta name="author" content="Steve Rhodes"
         <meta name="robots" content="noindex, nofollow"
     </head>
     <body >
     <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-secondary">
   <div class="container">
-    <a class="navbar-brand" href="/home">COSC 4806</a>
+    <a class="navbar-brand" href="/home">Hollywood</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/reminders">Reminders</a>
-        </li>
         <?php if (isset($_SESSION['admin'])) { ?>
           <li class="nav-item">
             <a class="nav-link" href="/reports">Reports</a>
@@ -38,9 +31,15 @@ if (!isset($_SESSION['auth'])) {
         <li class="nav-item">
           <a class="nav-link" href="/contact">Contact</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/logout">Logout</a>
-        </li>
+        <?php if (isset($_SESSION['auth'])) { ?>
+          <li class="nav-item">
+            <a class="nav-link" href="/logout">Logout</a>
+          </li>
+        <?php } else { ?>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+        <?php } ?>
       </ul>
     </div>
   </div>
