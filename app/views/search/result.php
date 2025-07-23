82adcac03cb843683f2b6ae7d759dcf52e32312e
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-23 21:07:01 +0000

    formatted display of OMDB results to display particular fields and movie poster

--- app/views/search/result.php
+++ app/views/search/result.php
@@ -7,22 +7,41 @@
                 <h1 class="display-1">Hollywood Express</h1>
                 <br>
             </div>
-            <div>
-                <p class="display-6"><?php 
-                if (isset($_SESSION['movie'])) {
-                    echo "<pre>";
-                    print_r ($_SESSION['movie']);
-                } ?>                
-                </p>
-            </div>
+            
+<!-- display search results --> 
             <div class="col-lg-12 text-center">
-                <form action="/search/review" method="post">
-                    <div class="input-group mb-3">
-                        <button class="btn btn-secondary" type="submit" name="title" value="<?php echo $_SESSION['movie']['Title'] ?>">Read a review</button>
-                    </div>
-                </form>
-
+                    <p class="display-6 text-center"><?php echo $_SESSION['movie']['Title'] . ' (' . $_SESSION['movie']['Year'] . ')' ?></p>
             </div>
+            <div class="col-sm-4">
+                    <img src="<?php echo $_SESSION['movie']['Poster']?>" alt="Movie Poster" class="img-fluid">
+            </div>
+            <div class="col-sm-8">
+
+                    <p class="lead-1">Directed by: <?php echo $_SESSION['movie']['Director']?></p>
+                    <p class="lead-1">Starring: <?php echo $_SESSION['movie']['Actors']?></p>
+                    <p class="lead-1">Plot: <?php echo $_SESSION['movie']['Plot']?></p>
+                    <p class="lead-1">Rated: <?php echo $_SESSION['movie']['Rated']?></p>
+                    <p class="lead-1">IMDB Rating (/10): <?php
+                        $rating = $_SESSION['movie']['imdbRating'];
+                        for ($i = 0; $i < $rating; $i++) {
+                            echo "★";
+                        } ?>
+                    </p>
+                    <p class="lead-1">Box office revenue: <?php echo $_SESSION['movie']['BoxOffice']?></p>
+             
+            </div>
+
+<!-- review button -->            
+            <div class="row justify-content-center">
+                <div class="col-lg-12 ">
+                    <form action="/search/review" method="post">
+                        <div class="input-group mb-3">
+                            <button class="btn btn-secondary" type="submit" name="title" value="<?php echo $_SESSION['movie']['Title'] ?>">Read a review</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+<!-- display review -->
             <div class="col-lg-9 text-center" id="review">
                 <?php if (isset($_SESSION['review'])) {
                     echo "<pre>";
